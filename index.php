3c5dc1406f748da11980424b356477fc1f60637a
Sooraj K Babu <soorajkbabu@gmail.com>
2017-08-19 15:07:16 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!--
+  * MagicMirror UI v1.0
+-->
+<?php
+  //server code
+?>
+<html>
+<head>
+  <title>Smart Mirror - Home</title>
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <link href="https://fonts.googleapis.com/css?family=Quicksand:300" rel="stylesheet">
+</head>
+<body>
+
+  <div class="main-heading">
+    Hey handsome!
+  </div>
+
+  <div class="weather">
+    <script src="//www.powr.io/powr.js" external-type="html"></script>
+    <div class="powr-weather" id="f1667dee_1503133066"></div>
+  </div>
+
+  <div class="twitter">
+    <script src="//www.powr.io/powr.js" external-type="html"></script> 
+    <div class="powr-social-feed" id="dbc45f1e_1503133651"></div>
+  </div>
+
+</body>
+</html>
